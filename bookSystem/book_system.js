79fa7ba6deb24f7c963fa98efaa726460170f5bc
ingarskrafts <ingarskrafts@gmail.com>
2025-01-10 06:54:48 -0500

    message

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -19,3 +19,13 @@ function addBook() {
         alert('Please fill in all fields correctly.');
     }
 }
+
+function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+        <p><strong>Book Name: </strong> ${book.name}</p>
+        <p><strong>Author Name: </strong> ${book.authorName}</p>
+        <p><strong>Book Description: </strong> ${book.bookDescription}</p>
+        <p><strong>No. of Pages: </strong> ${book.pagesNumber} page(s)</p>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
